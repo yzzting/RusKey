9ec4279ad8755d8d3a105c8cf06279d7a9a75b9c
yzzting <yzz1995@aliyun.com>
2023-11-08 07:33:09 +0800

    feat: Refactor string slicing logic in StringCommand // todo not yet completed

--- src/func/string.rs
+++ src/func/string.rs
@@ -21,7 +21,6 @@ impl StringCommand {
         while let Some(part) = parts.next() {
             if part.starts_with('"') && !part.ends_with('"') {
                 value_parts.push(part[1..].to_string());
-                println!("value_parts: {:?}", value_parts);
                 while let Some(part) = parts.next() {
                     value_parts.push(part.to_string());
                     if part.ends_with('"') {
@@ -59,24 +58,44 @@ impl StringCommand {
     fn get_range(&self, parts: &mut SplitAsciiWhitespace, db: &mut Db) -> Result<String, &'static str> {
         let key = parts.next().unwrap();
         let start = match parts.next() {
-            Some(start_str) => match start_str.parse::<usize>() {
+            Some(start_str) => match start_str.parse::<isize>() {
                 Ok(start) => start,
                 Err(_) => return Err("GetRange Error: Invalid start value"),
             },
             None => return Err("GetRange Error: Start not specified"),
         };
         let end = match parts.next() {
-            Some(end_str) => match end_str.parse::<usize>() {
-                Ok(end) => end + 1,
+            Some(end_str) => match end_str.parse::<isize>() {
+                Ok(end) => end,
                 Err(_) => return Err("GetRange Error: Invalid end value"),
             },
             None => return Err("GetRange Error: End not specified"),
         };
 
         let key_value = self.get(key, db).unwrap();
-        let key_value_splice = &key_value[start..end];
+        let key_value_splice = StringCommand::slice_from_end(&key_value, start, end);
 
         Ok(key_value_splice.to_string())
+    }
+
+    fn slice_from_end(str: &str, start: isize, end: isize) -> String {
+        println!("str: {}", str);
+        let char_vec: Vec<char> = str.chars().collect();
+
+        let start  = if start < 0 {
+            (char_vec.len() as isize + start) as usize
+        } else {
+            start as usize
+        };
+
+        let end = if end < 0 || end < char_vec.len() as isize - 1 {
+            (end + 1) as usize
+        } else {
+            end as usize
+        };
+        println!("start: {}", start);
+        println!("end: {}", end);
+        char_vec[start..end].iter().collect::<String>()
     }
 }
 
