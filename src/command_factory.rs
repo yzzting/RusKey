b60897dad6d84ca9b452b17744c249c42e926ab4
yzzting <yzz1995@aliyun.com>
2023-11-27 07:16:05 +0800

    feat: the collection of commands is exported using init_commands

--- src/command_factory.rs
+++ src/command_factory.rs
@@ -1,6 +1,7 @@
 use crate::db::Db;
 use std::collections::HashMap;
 use std::str::SplitAsciiWhitespace;
+use crate::init_commands::{STRING_COMMANDS, HASHMAP_COMMANDS, EXPIRED_COMMANDS, UTILS_COMMANDS};
 use crate::func::string::StringCommand;
 use crate::func::hashmap::HashMapCommand;
 use crate::func::ping::PingCommand;
@@ -19,39 +20,30 @@ pub struct CommandFactory {
 impl CommandFactory {
     pub fn new() -> CommandFactory {
         let mut commands: HashMap<String, Box<dyn Command>> = HashMap::new();
+        // Config
+        commands.insert("config".to_string(), Box::new(ConfigCommand::new()));
+        // Ping
         commands.insert("ping".to_string(), Box::new(PingCommand {}));
 
         // String
-        commands.insert("append".to_string(), Box::new(StringCommand::new("append".to_string())));
-        commands.insert("decr".to_string(), Box::new(StringCommand::new("decr".to_string())));
-        commands.insert("decrby".to_string(), Box::new(StringCommand::new("decrby".to_string())));
-        commands.insert("get".to_string(), Box::new(StringCommand::new("get".to_string())));
-        commands.insert("getdel".to_string(), Box::new(StringCommand::new("getdel".to_string())));
-        commands.insert("getex".to_string(), Box::new(StringCommand::new("getex".to_string())));
-        commands.insert("getrange".to_string(), Box::new(StringCommand::new("getrange".to_string())));
-        commands.insert("set".to_string(), Box::new(StringCommand::new("set".to_string())));
+        for command in STRING_COMMANDS.iter() {
+            commands.insert(command.to_string(), Box::new(StringCommand::new(command.to_string())));
+        }
 
         // HashMap
-        commands.insert("hmset".to_string(), Box::new(HashMapCommand::new("hmset".to_string())));
-        commands.insert("hgetall".to_string(), Box::new(HashMapCommand::new("hgetall".to_string())));
+        for command in HASHMAP_COMMANDS.iter() {
+            commands.insert(command.to_string(), Box::new(HashMapCommand::new(command.to_string())));
+        }
 
-        // Config
-        commands.insert("config".to_string(), Box::new(ConfigCommand::new()));
         // Expired
-        commands.insert("expired".to_string(), Box::new(ExpiredCommand::new("expired".to_string())));
-        commands.insert("expireat".to_string(), Box::new(ExpiredCommand::new("expireat".to_string())));
-        commands.insert("pexpireat".to_string(), Box::new(ExpiredCommand::new("pexpireat".to_string())));
-        commands.insert("ttl".to_string(), Box::new(ExpiredCommand::new("ttl".to_string())));
-        commands.insert("pttl".to_string(), Box::new(ExpiredCommand::new("pttl".to_string())));
-        commands.insert("persist".to_string(), Box::new(ExpiredCommand::new("persist".to_string())));
+        for command in EXPIRED_COMMANDS.iter() {
+            commands.insert(command.to_string(), Box::new(ExpiredCommand::new(command.to_string())));
+        }
 
         // Utils
-        commands.insert("rename".to_string(), Box::new(UtilsCommand::new("rename".to_string())));
-        commands.insert("renamenx".to_string(), Box::new(UtilsCommand::new("renamenx".to_string())));
-        commands.insert("randomkey".to_string(), Box::new(UtilsCommand::new("randomkey".to_string())));
-        commands.insert("del".to_string(), Box::new(UtilsCommand::new("del".to_string())));
-        commands.insert("exists".to_string(), Box::new(UtilsCommand::new("exists".to_string())));
-        commands.insert("type".to_string(), Box::new(UtilsCommand::new("type".to_string())));
+        for command in UTILS_COMMANDS.iter() {
+            commands.insert(command.to_string(), Box::new(UtilsCommand::new(command.to_string())));
+        }
         CommandFactory { commands }
     }
 
--- src/init_commands.rs
+++ src/init_commands.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 
-const STRING_COMMANDS: [&str; 8] = ["append", "decr", "decrby", "get", "getdel", "getex", "getrange", "set"];
-const HASHMAP_COMMANDS: [&str; 2] = ["hmset", "hgetall"];
-const EXPIRED_COMMANDS: [&str; 6] = ["expired", "expireat", "pexpireat", "ttl", "pttl", "persist"];
-const UTILS_COMMANDS: [&str; 6] = ["rename", "renamenx", "randomkey", "del", "exists", "type"];
+pub const STRING_COMMANDS: [&str; 8] = ["append", "decr", "decrby", "get", "getdel", "getex", "getrange", "set"];
+pub const HASHMAP_COMMANDS: [&str; 2] = ["hmset", "hgetall"];
+pub const EXPIRED_COMMANDS: [&str; 6] = ["expired", "expireat", "pexpireat", "ttl", "pttl", "persist"];
+pub const UTILS_COMMANDS: [&str; 6] = ["rename", "renamenx", "randomkey", "del", "exists", "type"];
 
 pub fn init_commands() -> HashSet<String> {
     let mut commands_map: Vec<String> = Vec::new();
