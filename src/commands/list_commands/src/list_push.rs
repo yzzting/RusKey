9fccae51b05d17c8559af227a1de8c98afabe188
杨滋滋 <yzz1995@aliyun.com>
2023-12-30 15:54:39 +0800

    feat: add 'lpush' ListCommand

--- src/commands/list_commands/src/list_push.rs
+++ src/commands/list_commands/src/list_push.rs
@@ -1,6 +1,31 @@
+use rus_key_command_lib::get_parts;
+use rus_key_db::db::DataType;
 use rus_key_db::db::Db;
+use std::collections::VecDeque;
 use std::str::SplitAsciiWhitespace;
 
 pub fn list_push(parts: &mut SplitAsciiWhitespace, db: &mut Db) -> String {
-    return "list_push".to_string();
+    let (key, _) = get_parts(parts, false);
+    let mut value_vec: VecDeque<String> = VecDeque::new();
+    while let Some(value) = parts.next() {
+        value_vec.push_front(value.to_string());
+    }
+
+    let mut list = match db.get(&key) {
+        Some(list) => match list {
+            DataType::List(list) => list.clone(),
+            _ => return "wrong type".to_string(),
+        },
+        None => VecDeque::new(),
+    };
+
+    for value in value_vec {
+        list.push_front(value);
+    }
+    println!("{:?}", list);
+    let len = list.len();
+
+    db.set(key.to_string(), DataType::List(list));
+
+    len.to_string()
 }
---
+++ tests/test_list_command.rs
@@ -0,0 +1,109 @@
+use list_commands::list::ListCommand;
+use rus_key_db::db::{DataType, Db};
+use rus_key_trait::command_trait::Command;
+use std::collections::VecDeque;
+use std::error::Error;
+
+fn generate_vec_deque(value: Vec<&str>) -> VecDeque<String> {
+    let mut value_vec: VecDeque<String> = VecDeque::new();
+    for v in value {
+        value_vec.push_front(v.to_string());
+    }
+    value_vec
+}
+
+fn general_command(
+    db: &mut Db,
+    command_set: &ListCommand,
+    command_set_str: &str,
+) -> Result<String, Box<dyn Error>> {
+    let mut parts_set = command_set_str.split_ascii_whitespace();
+    let result_set = command_set.execute(&mut parts_set, db);
+    match result_set {
+        Ok(value) => Ok(value),
+        Err(e) => Err(e.into()),
+    }
+}
+
+fn assert_command(
+    db: &mut Db,
+    command_set: &ListCommand,
+    args: &str,
+    key: &str,
+    expected_result: &str,
+    expected_value: VecDeque<String>,
+) -> Result<(), Box<dyn Error>> {
+    let general_result = general_command(db, command_set, args)?;
+    if !key.is_empty() {
+        assert_eq!(general_result, expected_result);
+    }
+    if expected_value.is_empty() {
+        let actual_value = match db.get(key) {
+            Some(DataType::List(actual_value)) => actual_value,
+            _ => panic!("Key not found"),
+        };
+        assert_eq!(*actual_value, expected_value);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_list_push_command() -> Result<(), Box<dyn Error>> {
+    let mut db = Db::new();
+
+    let list_push_command = ListCommand::new("lpush".to_string());
+
+    let tests_case: Vec<(&str, &str, &str, VecDeque<String>, &ListCommand)> = vec![
+        (
+            "key1 value1 value2",
+            "key1",
+            "2",
+            generate_vec_deque(vec!["value1", "value2"]),
+            &list_push_command,
+        ),
+        (
+            "key1 value3 value4",
+            "key1",
+            "4",
+            generate_vec_deque(vec!["value3", "value4", "value1", "value2"]),
+            &list_push_command,
+        ),
+        (
+            "key2 value1",
+            "key2",
+            "1",
+            generate_vec_deque(vec!["value1"]),
+            &list_push_command,
+        ),
+        (
+            "key2 value2",
+            "key2",
+            "2",
+            generate_vec_deque(vec!["value2", "value1"]),
+            &list_push_command,
+        ),
+        (
+            "key3 value1 value1",
+            "key3",
+            "2",
+            generate_vec_deque(vec!["value1", "value1"]),
+            &list_push_command,
+        ),
+        (
+            "key4",
+            "key4",
+            "0",
+            generate_vec_deque(vec![]),
+            &list_push_command,
+        ),
+    ];
+    for (args, key, expected_result, expected_value, command) in tests_case {
+        println!(
+            "arg: {}, key: {}, expected_result: {}, expected_value: {:?}",
+            args, key, expected_result, expected_value
+        );
+        assert_command(&mut db, command, args, key, expected_result, expected_value)?;
+    }
+    Ok(())
+}
